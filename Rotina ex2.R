e600dde569a009c59f6825eea2beb28e7bacd25f
AlcineiAzevedo <65502006+AlcineiAzevedo@users.noreply.github.com>
2021-08-03 17:58:12 -0300

    Add files via upload

--- Rotina ex2.R
+++ Rotina ex2.R
@@ -1,1 +1,18 @@
-s
+remove(list=ls())
+setwd("C:/Users/AlcineiAzevedo/Desktop/Alcinei/Simeali")
+D=read.table("ex2.txt",head=TRUE)
+rownames(D)=D[,1]
+D=D[,-1]
+library(MultivariateAnalysis)
+ComponentesPrincipais(D)
+
+?ComponentesPrincipais.Misto
+data("Dados.Misto")
+
+NomeTrat=paste("Trat",1:nrow(Dados.Misto),sep="_")
+ComponentesPrincipais.Misto(Dados.Misto,NomeTrat = NomeTrat)
+?VariaveisCanonicas
+
+#Delineamento inteiramente casualizado (DIC)
+data("Dados.DIC")
+VariaveisCanonicas(Dados.DIC,1)
